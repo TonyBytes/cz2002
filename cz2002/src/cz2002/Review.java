81963dd0581f4df424681f0b3f35c49efba86bdc
TonyBytes <chenghaolu@Chenghaos-MacBook-Pro.local>
2015-11-04 13:10:03 +0800

    Add Movie
    should showingTime extend movie?

--- cz2002/src/cz2002/Review.java
+++ cz2002/src/cz2002/Review.java
@@ -3,5 +3,11 @@ package cz2002;
 public class Review {
 	public String name;
 	public String content;
+	private int rating;
+	
+	public int getRating(){
+		return rating;
+	}
 
+	
 }
--- cz2002/src/cz2002/Ticket.java
+++ cz2002/src/cz2002/Ticket.java
@@ -4,7 +4,6 @@ public class Ticket {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
 	}
-
 }
